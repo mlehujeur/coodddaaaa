d455caf747edf80ed4f9cced8bdbf2346840bed5
mlehujeur <maximilien.lehujeur@gmail.com>
2023-04-06 11:07:12 +0200

    hot fix in polyspace

--- coodddaaaa/utils.py
+++ coodddaaaa/utils.py
@@ -24,6 +24,18 @@ class Timer:
 
 def polyspace(xmin: float, xmax: float, nx: int, pwr: float):
     """a power low to refine resolution near zero"""
-    u = np.linspace(-1., 1., nx)
-    u = np.sign(u) * np.abs(u) ** pwr
-    return 0.5 * (u + 1.) * (xmax - xmin) + xmin
+    assert pwr > 0, ValueError(pwr)
+
+    tmin = np.sign(xmin) * np.abs(xmin) ** (1. / pwr)
+    tmax = np.sign(xmax) * np.abs(xmax) ** (1. / pwr)
+    t = np.linspace(tmin, tmax, nx)
+    return np.sign(t) * np.abs(t) ** pwr
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    with Timer('test'):
+        x = polyspace(-0.01, 0.03, 100, 2)
+
+    plt.plot(x)
+    plt.show()
